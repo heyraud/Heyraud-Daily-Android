09b0198a55d46948acb1e04f54084de776d1f1fa
Heyraud Towne <heyraud.towne@gmail.com>
2018-09-28 11:30:21 +0800

    constraintLayout例子添加动态改变

--- app/src/main/java/io/github/heyraud/daily/android/samples/constraint_layout/ConstraintLayoutActivity.java
+++ app/src/main/java/io/github/heyraud/daily/android/samples/constraint_layout/ConstraintLayoutActivity.java
@@ -1,6 +1,7 @@
 package io.github.heyraud.daily.android.samples.constraint_layout;
 
 import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
@@ -12,12 +13,15 @@ import android.support.transition.TransitionSet;
 import android.support.transition.TransitionValues;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import io.github.heyraud.daily.android.BasicActivity;
 import io.github.heyraud.daily.android.R;
@@ -28,6 +32,7 @@ import io.github.heyraud.daily.android.R;
  * @author aero.tang
  * @version 2018/9/17 21:34
  */
+@SuppressLint("SetTextI18n")
 public class ConstraintLayoutActivity extends BasicActivity {
     private TabLayout mTab;
     private ViewPager mPager;
@@ -161,25 +166,161 @@ public class ConstraintLayoutActivity extends BasicActivity {
     private View obtainMargins() {
         final View container = inflate(1);
         final ConstraintLayout item = container.findViewById(R.id.parent);
+        final Button a = item.findViewById(R.id.a);
+        final Button c = item.findViewById(R.id.c);
+        final Button d = item.findViewById(R.id.d);
 
+        final Button btnA = container.findViewById(R.id.btn_a);
+        final Button btnC = container.findViewById(R.id.btn_c);
 
-        return item;
+        final ConstraintSet set = new ConstraintSet();
+        set.clone(item);
+
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (a.getVisibility() == View.VISIBLE) {
+                    set.setVisibility(a.getId(), View.GONE);
+                } else {
+                    set.setVisibility(a.getId(), View.VISIBLE);
+                }
+
+                TransitionManager.beginDelayedTransition(item);
+                set.applyTo(item);
+            }
+        });
+
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (c.getVisibility() == View.VISIBLE) {
+                    set.setVisibility(c.getId(), View.GONE);
+
+                    set.setGoneMargin(d.getId(), ConstraintSet.START,
+                            (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 112, getResources().getDisplayMetrics()));
+                } else {
+                    set.setVisibility(c.getId(), View.VISIBLE);
+                }
+
+                TransitionManager.beginDelayedTransition(item);
+                set.applyTo(item);
+            }
+        });
+        return container;
     }
+
+    private int biasH = 50;
+    private int biasV = 50;
 
     private View obtainCenteringPositioning() {
         final View container = inflate(2);
         final ConstraintLayout item = container.findViewById(R.id.parent);
+        final Button a = item.findViewById(R.id.a);
 
+        final SeekBar seekBiasH = container.findViewById(R.id.seek_bias_h);
+        final SeekBar seekBiasV = container.findViewById(R.id.seek_bias_v);
+        seekBiasH.setMax(100);
+        seekBiasH.setProgress(biasH);
+        seekBiasV.setMax(100);
+        seekBiasV.setProgress(biasV);
 
-        return item;
+        final ConstraintSet set = new ConstraintSet();
+        set.clone(item);
+
+        SeekBar.OnSeekBarChangeListener changeListener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (seekBar.getId() == seekBiasH.getId()) {
+                    biasH = progress;
+                } else if (seekBar.getId() == seekBiasV.getId()) {
+                    biasV = progress;
+                }
+
+                StringBuilder builder = new StringBuilder();
+                builder.append("H Bias ")
+                        .append(biasH)
+                        .append("%")
+                        .append("\n")
+                        .append("V Bias ")
+                        .append(biasV)
+                        .append("%");
+                if (biasH == 50 && biasV == 50) {
+                    builder.append("\nCenter");
+                }
+
+                a.setText(builder.toString());
+
+                set.setHorizontalBias(a.getId(), biasH / 100f);
+                set.setVerticalBias(a.getId(), biasV / 100f);
+                set.applyTo(item);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
+
+        seekBiasH.setOnSeekBarChangeListener(changeListener);
+        seekBiasV.setOnSeekBarChangeListener(changeListener);
+
+        return container;
     }
+
+    private int angleValue = 30;
+    private int radiusValue;
 
     private View obtainCircularPositioning() {
         final View container = inflate(3);
         final ConstraintLayout item = container.findViewById(R.id.parent);
+        final TextView a = item.findViewById(R.id.a);
+        final View anchor = item.findViewById(R.id.center);
 
+        final SeekBar angle = container.findViewById(R.id.seek_circle_angle);
+        final SeekBar radius = container.findViewById(R.id.seek_circle_radius);
+        angle.setMax(360);
+        angle.setProgress(angleValue);
+        radius.setMax(500);
+        radiusValue = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics());
+        radius.setProgress(radiusValue);
 
-        return item;
+        final ConstraintSet set = new ConstraintSet();
+        set.clone(item);
+
+        SeekBar.OnSeekBarChangeListener changeListener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (seekBar.getId() == angle.getId()) {
+                    angleValue = progress;
+                } else {
+                    radiusValue = progress;
+                }
+
+                a.setText(angleValue / 30 + "");
+                set.constrainCircle(a.getId(), anchor.getId(), radiusValue, angleValue);
+                set.applyTo(item);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
+
+        angle.setOnSeekBarChangeListener(changeListener);
+        radius.setOnSeekBarChangeListener(changeListener);
+
+        return container;
     }
 
     private class ConstraintAdapter extends PagerAdapter {
